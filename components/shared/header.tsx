f807800f55cf8043fd3a722cd644ce45098f09bf
Salman <himishevsa@intech.rshb.ru>
2024-07-19 17:34:03 +0300

    feat(header): add shooping button

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils';
 import React from 'react';
 import { Container } from './container';
 import Image from 'next/image';
-import { PersonStanding, ShoppingCart, User } from 'lucide-react';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
 import { Button, Input } from '../ui';
 
 interface Props {
@@ -23,7 +23,17 @@ export const Header: React.FC<Props> = ({ className }) => {
             <div><Input /></div>
             <div className="flex gap-3">
                 <Button className='flex items-center gap-1' variant="outline"><User size={16} /> Войти</Button>
-                <Button variant="outline"><ShoppingCart /></Button>
+                <div>
+                    <Button className='group relative'>
+                        <b>520 $</b>
+                        <span className='h-full w-[1px] bg-white/30 mx-3' />
+                        <div className='flex items-center gap-1 transition duration-300 group-hover:opacity-0'>
+                            <ShoppingCart className='h-4 w-4 relative' strokeWidth={2} />
+                            <b>3</b>
+                        </div>
+                        <ArrowRight className='w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0' />
+                    </Button> 
+                </div>
             </div>
         </Container>
     </header>
